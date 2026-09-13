46341030992dff5f03d8b73b500c03380f7ffc24
Lancea <lanceabuela@gmail.com>
2026-09-13 18:00:21 +0800

    test: implemented NotificationRepositoryTest and refactored Notification entity

--- src/main/java/dev/romulus_lanceues/tanaw_api/notification/Notification.java
+++ src/main/java/dev/romulus_lanceues/tanaw_api/notification/Notification.java
@@ -10,11 +10,13 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.Instant;
 import java.util.UUID;
 
 @Entity
+@EntityListeners(AuditingEntityListener.class)
 @Table(
         name = "notifications",
         uniqueConstraints = {
