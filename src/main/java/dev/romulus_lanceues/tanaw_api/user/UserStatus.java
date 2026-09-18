8b00713c77fe56523f77d41f7d3f82a8d451822c
Lancea <lanceabuela@gmail.com>
2026-09-18 16:39:57 +0800

    docs(user): add documentation for user controller

--- src/main/java/dev/romulus_lanceues/tanaw_api/user/UserStatus.java
+++ src/main/java/dev/romulus_lanceues/tanaw_api/user/UserStatus.java
@@ -1,6 +1,12 @@
 package dev.romulus_lanceues.tanaw_api.user;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+
+@Schema(description = "Account status indicating whether the user is active or disabled")
 public enum UserStatus {
-        ACTIVE,
-        DISABLED
+    @Schema(description = "User account is active and can receive alerts")
+    ACTIVE,
+
+    @Schema(description = "User account has been deactivated")
+    DISABLED
 }
